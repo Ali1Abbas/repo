5eea6180bcffbfd0537323f2d80aa92981469b00
sana2024 <sanaomervi01@gmail.com>
2023-10-22 11:20:52 +0300

    description

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    print('hello');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
